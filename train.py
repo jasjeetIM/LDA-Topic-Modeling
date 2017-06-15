5e4b02e185ae9701629290f077271b0655cbee49
Jay <jasjeetim@gmail.com>
2017-06-14 20:51:44 -0700

    set default params

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from database import Database
 from lda import LDA
 from argparse import ArgumentParser
 
-def run_lda(data_dir, num_topics, use_mini_batches, batch_size, epochs,visualize, model_file, create_dict, dict_file, load_dbs):
+def run_lda(data_dir, num_topics, use_mini_batches, batch_size, epochs, model_file, create_dict, dict_file, load_dbs):
   """ Run training and display test results if visualize is true
 
   Args:
@@ -14,7 +14,6 @@ def run_lda(data_dir, num_topics, use_mini_batches, batch_size, epochs,visualize
     num_topics(int): Number of topics to train the model on
     batch_size(int): Size of mini batches used to train the model
     epochs(int): Number of epochs to train the data for on the train set
-    visualize(bool): visuallize results of the trained model
     model_file(str): saved model file to continue training on
     create_dict(bool): create dictionary from data or load dict from a file
     dict_file(str): dict_file path to load dictionary from 
@@ -90,11 +89,10 @@ if __name__ == '__main__':
   parser.add_argument("--use_mini_batches", help="bool for training on mini batches or entire dataset", action='store_true')
   parser.add_argument("--batch_size", help="Provide the batch_size during training", default=1000, type=int)
   parser.add_argument("--num_epochs", help="Provide the number of epochs to train for", default=1, type=int)
-  parser.add_argument("--num_topics", help="Provide the number of topics to run lda", default=10, type=int)
+  parser.add_argument("--num_topics", help="Provide the number of topics to run lda", default=4, type=int)
   parser.add_argument("--model_file", help="Provide the path to the model object that needs to be trained", default='', type=str)
-  parser.add_argument("--dict_file", help="Provide the path to the dict object that needs to be loaded", default='./dict/dictionary', type=str)
-  parser.add_argument("--visualize", help="1 means visualize training and results, 0 means don't visualize", action='store_true')
-  parser.add_argument("--create_dict", help="Load a dictionary or create a new dictionary", action='store_true')
-  parser.add_argument("--load_dbs", help="Load a database or create a new database", action='store_false')
+  parser.add_argument("--dict_file", help="Provide the path to the dict object that needs to be loaded", default='', type=str)
+  parser.add_argument("--create_dict", help="Load a dictionary or create a new dictionary", action='store_false')
+  parser.add_argument("--load_dbs", help="Load a database or create a new database", action='store_true')
   args = parser.parse_args()
-  run_lda(args.data_directory, args.num_topics, args.use_mini_batches, args.batch_size, args.num_epochs, args.visualize, args.model_file, args.create_dict, args.dict_file, args.load_dbs)
+  run_lda(args.data_directory, args.num_topics, args.use_mini_batches, args.batch_size, args.num_epochs,  args.model_file, args.create_dict, args.dict_file, args.load_dbs)
